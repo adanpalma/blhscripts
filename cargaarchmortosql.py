bdcbea346ba9d2f5f28bce2032ba09d65a45ca61
Adan Palma <adanpalma@hotmail.com>
2021-10-27 16:22:26 -0500

    Add files via upload

--- cargaarchmortosql.py
+++ cargaarchmortosql.py
@@ -1,12 +1,11 @@
 from  pathlib import Path
+import pyodbc
 
 filename = Path("data\\archmoro-pan-921.txt")
 
 with open(filename) as filename:
     #Salto  lineas de titulos
     next(filename)
-
-
 
     for row in filename:
         #en archmoro hay prestamos que tiene que se pegan a la cartera y quitan
@@ -28,6 +27,10 @@ with open(filename) as filename:
         registro = dict(zip(("Prestamo","cartera","periodo","delinquency","saldoactual","sprogramado","ipagado","tasaint","cobrador"), (prestamo,cartera,periodo,delinquency,saldoactual,sprogramado,ipagado,tasainteres,cobrador)))
         print(registro)
         #TODO: Guardar este dict en una lista de al menos 50K filas una vez lleno,guardar ese arreglo de dict en una tabla en SQL SERVER
+
+        # dsn= odbcR_to_Sql_RiskPan  es un dsn que debes crear en Windows
+        conn = pyodbc.connect( "dsn=odbcR_to_Sql_RiskPan")
+
 
         break
 

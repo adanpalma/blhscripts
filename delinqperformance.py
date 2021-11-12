b40b7dd2da44890984b9f1b02797389ef0840e60
adan.palma <adanpalma@hotmail.com>
2021-11-12 17:36:39 -0500

    Scripts delinq_vintage.ipynb y delinqperformance.py para procesar los vintages y graficarlos

---
+++ delinqperformance.py
@@ -0,0 +1,62 @@
+import sqlalchemy
+from pandas import DataFrame
+from sqlalchemy.engine import URL
+import pandas as pd
+import plotly.express as px
+
+from timeit import default_timer as timer
+
+# pyodbc parameter for conections to database
+driver = '{SQL Server}'
+server = '10.10.20.37'
+database = 'RISKPAN'
+trust_connection = 'yes'
+
+db_flavor = 'mssql+pyodbc'
+
+# Conection String
+conn_string = f'DRIVER={driver};'
+conn_string += f'SERVER={server};'
+conn_string += f'DATABASE={database};'
+conn_string += f'TRUSTED_CONNECTION={trust_connection}'
+
+# Sqlalchemy engine connection URL
+conn_url = URL.create(db_flavor, query={"odbc_connect": conn_string})
+
+##Create engine connection
+engine = sqlalchemy.create_engine(conn_url)
+
+# Read periodos_transcurridos_x_coshechas from sql to pandas.data frame
+df_periodosbyvintages = pd.read_sql_table('periodos_transcurridos_x_cosecha', engine)
+
+# Agrego campo para crear el campo acumulado
+df_periodosbyvintages["Hipotecas_deemed_acumulado"] = 0
+df_periodosbyvintages["Hipotecas_default_acumulado"] = 0
+df_periodosbyvintages["personal_deemed_acumulado"] = 0
+df_periodosbyvintages["personal_default_acumulado"] = 0
+
+# Se lee el deemdefault de cada cosecha, se une al dataframe periodos y se genera un cumsum()
+df_deemed_total: DataFrame = pd.DataFrame()
+primera_vez = 1
+for vvintage in [2019, 2020, 2021]:
+    qry_deemed = """ select * from DEEMED_DEFAULT_POR_COSECHAS_HIPO where vintage = {} order by 2""".format(vvintage)
+    df_deemed_defaults_vintage = pd.read_sql(qry_deemed, engine)
+    if primera_vez:
+        df_deemed_total = df_deemed_defaults_vintage
+        primera_vez = 0
+    else:
+        df_deemed_total = df_deemed_total.append(df_deemed_defaults_vintage)
+
+df_periodosbyvintages = pd.merge(df_periodosbyvintages, df_deemed_total, how='left', on=['vintage', 'period'])
+df_periodosbyvintages.fillna(0, inplace=True)
+df_periodosbyvintages['Hipotecas_deemed_acumulado'] = df_periodosbyvintages.groupby('vintage')['percentage_deemed_defaulted'].cumsum()
+
+del df_deemed_defaults_vintage
+del df_deemed_total
+
+df_plotear = df_periodosbyvintages[['vintage','period','Hipotecas_deemed_acumulado']]
+
+plt = px.line(df_plotear,x='period',y='Hipotecas_deemed_acumulado',color='vintage')
+plt.show()
+
+
